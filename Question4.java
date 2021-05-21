343b25210d6c17515df64a86d11ac6243ce8aa24
Suchitra786 <83940674+Suchitra786@users.noreply.github.com>
2021-05-21 12:32:28 +0530

    Add files via upload

---
+++ Question4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Question4 {
+public static void main(String[] args) {
+    String s[]= {"goa","Iceland","Banglore"};
+    HashMap<String,String> n=new HashMap<> ();
+    for(String sr:s) {
+        n.put(sr.substring(0,3).toUpperCase(),sr);
+    }
+    System.out.print(n);
+}
+}
