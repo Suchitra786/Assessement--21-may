2ecf442c4074c8b7d0c463f5bcdba5b4f729a149
Suchitra786 <83940674+Suchitra786@users.noreply.github.com>
2021-05-21 12:43:03 +0530

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Question4 {
 public static void main(String[] args) {
-    String s[]= {"goa","Iceland","Banglore"};
+    String s[]= {"goa","kerala","gujurat"};
     HashMap<String,String> n=new HashMap<> ();
     for(String sr:s) {
         n.put(sr.substring(0,3).toUpperCase(),sr);
